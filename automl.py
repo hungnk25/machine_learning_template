8ce325b805d947af8ed7013289b4ddfb188ec88f
mb <mb@mbs-MacBook-Pro.local>
2022-09-16 17:03:59 -0700

    Add xgboost into auto ml module

--- automl.py
+++ automl.py
@@ -12,7 +12,7 @@ from hyperopt import STATUS_OK, Trials, fmin, hp, tpe, space_eval
 from hyperopt.pyll import scope
 from sklearn.model_selection import StratifiedKFold, cross_val_score, GridSearchCV, RandomizedSearchCV
 from lightgbm import LGBMClassifier
-
+from xgboost import XGBClassifier
 import time as tm
 
 import logging
@@ -30,7 +30,11 @@ LGBM_MAX_DEPTH = 25 #maximum tree depth for LightGBM
 EVAL_METRIC_LGBM_REG = 'mae' #LightGBM regression metric. Note that 'rmse' is more commonly used 
 EVAL_METRIC_LGBM_CLASS = 'auc'#LightGBM classification metric
 
-
+#XGBOOST PARAMETERS
+XGB_MAX_LEAVES = 2**12 #maximum number of leaves when using histogram splitting
+XGB_MAX_DEPTH = 25 #maximum tree depth for XGBoost
+EVAL_METRIC_XGB_REG = 'mae' #XGBoost regression metric
+EVAL_METRIC_XGB_CLASS = 'auc' #XGBoost classification metric
 
 class AutoLGB():
     
@@ -93,6 +97,78 @@ class AutoLGB():
             self.tune(X, y)
         logger.info(f"Fitting model with input data")
         model = LGBMClassifier(**self.best_params)
+        model.fit(X, y)
+        acc = accuracy_score(y, model.predict(X))
+        logger.info(f"Model training accuracy: {acc}")
+        self.model = model
+
+
+    def predict(self, X):
+        if not self.model:
+            logger.info("There is no model. Tune and train your model first")
+            return []
+        y_pred = self.model.predict(X)
+        return y_pred
+
+
+class AutoXGB():
+    
+    default_xgb_space = {
+
+        'n_estimators': scope.int(hp.quniform('n_estimators', 100, 1000, 1)),
+        'learning_rate' : hp.loguniform('learning_rate', np.log(0.005), np.log(0.2)),
+        'reg_alpha' : hp.uniform('reg_alpha', 0, 5),
+        'reg_lambda' : hp.uniform('reg_lambda', 0, 5),
+        'max_depth':  scope.int(hp.quniform('max_depth', 2, XGB_MAX_DEPTH, 1)),
+        'min_child_weight': hp.quniform('min_child_weight', 1, 6, 1),
+        'subsample': hp.quniform('subsample', 0.5, 1, 0.05),
+        'gamma': hp.quniform('gamma', 0.5, 1, 0.05),
+        'colsample_bytree': hp.quniform('colsample_bytree', 0.5, 1, 0.05),
+        'eval_metric': 'error',
+        'objective': 'binary:logistic',
+        'nthread': -1,
+        'booster': 'gbtree',
+        'tree_method': 'exact'
+    }
+
+    def __init__(self, space=default_xgb_space, n_eval=10):
+        self.n_eval = n_eval
+        self.space = space
+        self.model = None
+        self.best_params = None
+
+    def tune(self, X, y):
+
+        def objective(params):
+            kfold = StratifiedKFold(n_splits=5, shuffle=True, random_state=0)
+            model = XGBClassifier(**params)
+            scores = cross_val_score(model, X, y, cv=kfold, scoring="accuracy", n_jobs=-1)
+            #y_pred = model.predict(x_val)
+            score = scores.mean()
+            # TODO: Add the importance for the selected features
+            #print("\tScore {0}".format(score))
+            # The score function should return the loss (1-score)
+            # since the optimize function looks for the minimum
+            loss = 1 - score
+            return {'loss': loss, 'status': STATUS_OK}
+    
+        logger.info(f"Starting optimization process for model XgBoost Classifier")
+        st = tm.time()
+        
+        trials = Trials() 
+        best = fmin(objective, self.space, algo=tpe.suggest, max_evals=self.n_eval, trials=trials)
+        self.best_params = space_eval(self.space, best)
+        logger.info(f"Best params for model: {self.best_params}")
+        logger.info(f"Time taken to run for: {(tm.time() - st)/60:.1f}(mins)")
+
+        #return self.best_params
+
+    def fit(self, X, y):
+        if not self.best_params:
+            logger.info(f"Haven't run model tuning yet. Starting tuning to find best params")
+            self.tune(X, y)
+        logger.info(f"Fitting model with input data")
+        model = XGBClassifier(**self.best_params)
         model.fit(X, y)
         acc = accuracy_score(y, model.predict(X))
         logger.info(f"Model training accuracy: {acc}")
